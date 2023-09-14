d5095d3acdf3391a326a067feae2931262f05c7b
lucatdat <luc.duperron@laposte.net>
2023-09-14 09:43:41 +0200

    ajout d'une application Web fonctionnelle en Python

---
+++ Partie9/EntityTodo.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Todo:
+    id:int=0
+    title:str=''
+    completed:bool=False
--- Partie9/app.py
+++ Partie9/app.py
@@ -1,16 +1,39 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
+from TodoDAO import TodoDAO
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
 
-    l=[
+    dao = TodoDAO("todos_db.db")
+
+    listeChargeeDAO = dao.findAll()
+    
+    return render_template('todo_list.html', todos=listeChargeeDAO)
+
+@app.route("/delete/<id>")
+def delete(id):
+    dao = TodoDAO("todos_db.db")
+    dao.delete(id)
+    return redirect('/')
+
+@app.route("/sale")
+def sale():
+    dao = TodoDAO("todos_db.db")
+
+    listeManuelle=[
         {"name":"Dup", "firstname":"Luc"},
         {"name":"Doe", "firstname":"John"},
     ]
+    listeChargeedeDAO = dao.findAll()
+    
     html=""
-    for item in l:
-        html+=f"<tr><td>{item['firstname']}</td><td>{item['name']}</td></tr>"
-    return f"<table>{html}</table>"
 
+    for item in listeManuelle:
+        html+=f"<tr><td>{item['firstname']}</td><td>{item['name']}</td></tr>"
+
+    for todo in listeChargeedeDAO:
+        html+=f"<tr><td>{todo.title}</td><td>{todo.completed}</td></tr>"
+
+    return f"<table>{html}</table>"
